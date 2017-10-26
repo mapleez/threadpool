b135a2e19a209fdf0de0b39cd280baff5b3bc684
mapleez <kukunanhai_5207@126.com>
2017-10-26 10:59:05 +0800

    Edit compiling script. cleaning script. Will be replaced by GNU standard builder.

---
+++ test/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ROOT=`dirname $0`
+rm $ROOT/../test/*.o $ROOT/../src/*.o
+
+
+
--- test/compile.sh
+++ test/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -c ../src/testmod.c -g
+gcc -c ../src/testmod.c -o ../src/testmod.o -g
 gcc -I../src -c ./test_testmod.c -g
 gcc -L/usr/local/lib -lcunit -I../src  -o test_testmod test_testmod.o ../src/testmod.o -g
