930420ca8b6df1827c2e69693bd56957327ca333
mapleez <kukunanhai_5207@126.com>
2017-11-03 10:53:44 +0800

    Add tests subdir. It meight not  work.

---
+++ tests/t_ez_job.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "ez_job.h"
+#include <assert.h>
+
+void _func1 (void* args) {
+	// Empty...
+}
+
+
+/* Check creation. */
+int _t_ez_job_create1 () {
+	pez_job job = ez_job_create (_func1, NULL);
+
+	/* Not null */
+	if (job != NULL) {
+		ez_job_destroy (&job);
+		assert (job == NULL);
+	}
+
+	/* Shell return NULL. */
+	job = ez_job_create (NULL, NULL);
+	assert (job == NULL);
+
+	job = ez_job_create (_func1, (void*) _func1);
+	if (job != NULL) {
+		ez_job_destroy (&job);
+		assert (job == NULL);
+	}
+
+	return 1;
+}
+
+/* Main entry */
+int main (int argc, char* argv []) {
+	if (
+			_t_ez_job_create1 ()
+	) puts ("t_ez_job ... ok!");
+	else
+		puts ("t_ez_job ... error!");
+
+	return 0;
+}
